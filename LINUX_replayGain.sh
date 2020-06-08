22e4bc2487a5996dcd400c32153865e1c9f0a4a1
Gautier Vanderslyen <gautier.vanderslyen@gmail.com>
2020-06-08 10:27:32 +0200

    bug for flac solved reviewed console output to be more readable added force recalculate option

--- LINUX_replayGain.sh
+++ LINUX_replayGain.sh
@@ -6,37 +6,77 @@ pathPattern="Z"
 mount_name="/mnt/replayGainScript"
 mount_user=""
 mount_user_default="gautier"
+forceRecalculation
 
 
 executeMP3(){
-	printf ">>> album for mp3 is $1 \n"
-	mp3gain -a -c "$1"/*.mp3
+	printf ">>> execute replaygain (mp3gain) \n"
+	if [[ "$forceRecalculation" -eq "1" ]]; then
+		printf "Force recalculation...\n"
+		mp3gain -a -c -f -s r "$1"/*.mp3 
+	else
+		mp3gain -a -c -f "$1"/*.mp3
+	fi
 }
 
 executeFLAC(){
-	printf ">>> album for flac is $1 \n"
-	metaflac --add-replay-gain "$1"/*.flac
+	printf ">>> execute metaflac \n"
+	currentDir=$(pwd)
+	cd "$1"
+
+	if [[ "$forceRecalculation" -eq "1" ]]; then
+		printf "Force recalculation : removing all replayGain tags...\n"
+		metaflac --remove-replay-gain ./*.flac
+	fi
+
+
+	containsReplaygainTags=1
+	for f in *.flac; do
+  		#echo "File -> $f"
+  		trackGain=$(metaflac "$f" --show-tag=REPLAYGAIN_TRACK_GAIN)
+  		albumGain=$(metaflac "$f" --show-tag=REPLAYGAIN_ALBUM_GAIN)
+  		
+  		if [[ -z $trackGain || -z $albumGain ]]; then
+  			containsReplaygainTags=0
+  			break
+	  	else
+	  		printf ">"$trackGain"<->"$albumGain"<"
+	  		printf "\n"
+  		fi
+	done
+	
+	if [[ "$containsReplaygainTags" -eq "1" ]] ; then
+  		printf "  => No need to replaygain\n"	
+  	else
+  		printf "  => Need to replaygain\n"
+		metaflac --add-replay-gain --no-utf8-convert --show-tag=TITLE ./*.flac
+  	fi
+		
+
+	cd $currentDir
 }
 
 executeFolder(){
 	album=${1}
-	foundSongs=false	
+	printf "\n\nChecking album: $album\n"
 	sizeMP3=$(find "$album" -maxdepth 1 -mindepth 1 -name "*.mp3" -type f | wc -l)
 	sizeFLAC=$(find "$album" -maxdepth 1 -mindepth 1 -name "*.flac" -type f | wc -l)
 	if [[ $sizeMP3 > 0 && $sizeFLAC > 0 ]]; then
-		printf "\n\t !!!! Mix of format, the following folder should be checked first : \n"
-		printf "\t\t$album\n"
+		printf "!!!! Mix of format, the following folder should be checked first : \n"
+		printf "\t$album\n"
 		echo "Mix of music formats : $album" >> ${tmpFile}
 	elif [[ $sizeMP3 == 0 && $sizeFLAC == 0 ]]; then
-		printf "\n\t !!!! No song found in the right format, the following folder should be checked first : \n"
-		printf "\t\t$album\n"
+		printf "!!!! No song found in the right format, the following folder should be checked first : \n"
+		printf "\t$album\n"
 		echo "No song of music format : $album" >> ${tmpFile}
 	elif [[ $sizeMP3 > 0 ]]; then
-		printf "$album\n"
+		printf "This album contains MP3\n"
 		executeMP3 "$album"
 	elif [[ $sizeFLAC > 0 ]]; then
-		executeFLAC "$album\n"
+		printf "This album contains FLAC\n"
+		executeFLAC "$album"
 	fi
+	printf "\n"
 }
 
 replaygain(){
@@ -46,6 +86,7 @@ replaygain(){
 	export -f executeFolder
 	export -f executeMP3
 	export -f executeFLAC
+	export forceRecalculation
 	export tmpFile
 	find $path -maxdepth 1 -mindepth 1 -type d -exec bash -c 'executeFolder "$0"' {} \;
 }
@@ -66,6 +107,11 @@ readInputs(){
 	read -p "   Path to search for: "$'\n'$"(default is 'Z', but an example is 'Z' or 'A' or 'Meta')"$'\n'$">" pathPattern;
 	if [[ "$pathPattern" == "" ]]; then
 		pathPattern="Z";
+	fi
+	
+	read -p "   Force recalcuation: "$'\n'$"(default is '0', choose '0' or '1')"$'\n'$">" forceRecalculation;
+	if [[ "$forceRecalculation" == "" ]]; then
+		forceRecalculation="0";
 	fi
 }
 
@@ -89,13 +135,13 @@ mountJ(){
 	printf "creating folder $mount_name\n"
 	mkdir $mount_name
 	
-#	mkdir /mnt/jupiter
 	mount -t cifs "$mount_path" "$mount_name" -o username="$mount_user",uid=gautier,gid=users
 	printf "<<< MOUNT FINISHED : $mount_name\n"
 }
 
 createTemp(){
 	printf "creating temp file $tmpFile"
+	rm /tmp/replayGainScript.*
 	tmpFile=$(mktemp /tmp/replayGainScript.XXXXXXXXX)
 }
 
@@ -105,7 +151,7 @@ readSummary(){
 	printf "********************************\n"
 	cat $tmpFile
 	printf "********************************\n"
-	printf "deleting temp file $tmpFile"
+	printf "deleting temp file $tmpFile\n"
 	rm $tmpFile
 }
 
@@ -116,10 +162,14 @@ if [[ $EUID -ne 0 ]]; then
    	printf "This script must be run as root type: sudo ./<script>\n" 
    	exit 1
 else
-	apt install cifs-utils
+	apt install cifs-utils flac mp3gain
 
 	readInputs
 	mountJ
+	
+	#mount_name="/home/gautier/Music/nas"
+	#pathPattern="Adagi"
+	
 	createTemp
 	replaygain
 	readSummary
